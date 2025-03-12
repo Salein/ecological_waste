f6533d4ecfaab18fd8703d63bf4d798f248a25dc
Sallein <saleinkyl@gmail.com>
2025-03-12 19:33:57 +0300

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <input placeholder="Строка Поиска Обьектов" />
+        <p>3142701 Отходы бетона</p>
+        <p>Местоположение обьекта</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
